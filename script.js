19f1de8dd0abe4e1be390d4c6e8f8788ccd15948
RaveNU3 <123623157+RaveNU3@users.noreply.github.com>
2023-08-28 11:07:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ playerSelection = prompt(`Rock, Paper, or Scissors?`).toLowerCase()
 computerSelection = getComputerChoice().toLowerCase()
 
 if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
-    alert(`player Chose ${playerSelection} and computer chose ${computerSelection} you win!!`)
+    alert(`You chose "${playerSelection}" and computer chose "${computerSelection}" you win!!`)
 } else if(playerSelection == "scissors" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors") {
-    alert(`player Chose ${playerSelection} and computer chose ${computerSelection} you lose!!`)
+    alert(`player chose "${playerSelection}" and computer chose "${computerSelection}",  you lose noob!!`)
 } else if (playerSelection == computerSelection) {
-    alert(`player Chose ${playerSelection} and computer chose ${computerSelection} It's a tie!!`)
+    alert(`You chose "${playerSelection}" and computer chose "${computerSelection}".  It's a tie!!`)
 } else {
   alert(`invalid input please type Rock, paper or scissors,`)
 }
