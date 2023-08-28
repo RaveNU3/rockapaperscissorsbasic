1053369744796be2f674ee401762067fc99d2670
RaveNU3 <123623157+RaveNU3@users.noreply.github.com>
2023-08-28 11:14:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ computerSelection = getComputerChoice().toLowerCase()
 if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
     alert(`You chose "${playerSelection}" and computer chose "${computerSelection}" you win!!`)
 } else if(playerSelection == "scissors" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors") {
-    alert(`player chose "${playerSelection}" and computer chose "${computerSelection}",  you lose noob!!`)
+    alert(`You chose "${playerSelection}" and computer chose "${computerSelection}",  you lose noob!!`)
 } else if (playerSelection == computerSelection) {
     alert(`You chose "${playerSelection}" and computer chose "${computerSelection}".  It's a tie!!`)
 } else {
-  alert(`invalid input please type Rock, paper or scissors,`)
+  alert(`Invalid input please type Rock, Paper or Scissors,`)
 }
